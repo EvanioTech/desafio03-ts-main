4058e43d36ae6021e0b5063cf23e02730e078cd0
Evanio Tech <evanioshark@gmail.com>
2026-04-23 09:06:12 -0300

    fix: ajuste no teste de validação das credenciais

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -2,14 +2,15 @@ import { login } from "./login"
 
 describe('login', () => {
 
-    const mockEmail = 'nath@dio.bank'
-    it('Deve exibir um alert com boas vindas caso o email seja válido', async() => {
-        const response = await login(mockEmail)
+    const mockEmail = 'matias@dio.bank'
+    const mockPassword = '123456'
+    it('Deve exibir um alert com boas vindas caso o email e senha sejam válidos', async() => {
+        const response = await login(mockEmail, mockPassword)
         expect(response).toBeTruthy()
     })
 
-    it('Deve exibir um erro caso o email seja inválido', async() => {
-        const response = await login('email@invalido.com')
+    it('Deve exibir um erro caso o email ou senha seja inválido', async() => {
+        const response = await login('email@invalido.com', 'senhaerrada')
         expect(response).toBeFalsy()
     })
 })
